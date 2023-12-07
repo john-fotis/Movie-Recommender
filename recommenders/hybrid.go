89798a4d675667de78f1cf6ff8a8e8d13d2612ad
John Fotis <giannisfotis@gmail.com>
2023-12-07 17:55:55 +0200

    Change the way the similarity metrics are combined in hybrid mode

--- recommenders/hybrid.go
+++ recommenders/hybrid.go
@@ -16,22 +16,32 @@ func RecommendHybrid(cfg *config.Config, titles *map[int]model.MovieTitle, movie
 	fmt.Printf("Working with %d movie ratings.\n", totalRatings)
 	util.StartProfiling("hybrid")
 	finalSimilarMovies := make([]model.SimilarMovie, 0)
-	// Combine item-item, title & tag collaborative filtering and sort all
+	// Combine tag, title & item-item collaborative filtering and sort all
 	// result-slices for more efficient calulcations of average similarity.
-	movieCfg := config.Config{Similarity: cfg.Similarity, NumThreads: cfg.NumThreads}
-	similarMovies := findSimilarMovies(&movieCfg, cfg.Input, movies)
-	sort.SliceStable(similarMovies, func(i, j int) bool {
-		return similarMovies[i].MovieID < similarMovies[j].MovieID
-	})
-	titleCgf := config.Config{Recommendations: len(*titles), Similarity: cfg.Similarity, Input: cfg.Input}
-	similarMoviesByTitle := RecommendBasedOnTitle(&titleCgf, titles)
-	sort.SliceStable(similarMoviesByTitle, func(i, j int) bool {
-		return similarMoviesByTitle[i].MovieID < similarMoviesByTitle[j].MovieID
-	})
 	tagCfg := config.Config{Recommendations: len(*tags), Similarity: cfg.Similarity, Input: cfg.Input}
 	similarMoviesByTag := RecommendBasedOnTag(&tagCfg, tags)
 	sort.SliceStable(similarMoviesByTag, func(i, j int) bool {
 		return similarMoviesByTag[i].MovieID < similarMoviesByTag[j].MovieID
+	})
+	// Create a subset of movie titles, only keeping the movieIDs that are recommendable by Tag-based correlation
+	recommendableTitles := map[int]model.MovieTitle{cfg.Input: (*titles)[cfg.Input]}
+	for _, movie := range similarMoviesByTag {
+		recommendableTitles[movie.MovieID] = ((*titles)[movie.MovieID])
+	}
+	titleCgf := config.Config{Recommendations: len(*titles), Similarity: cfg.Similarity, Input: cfg.Input}
+	similarMoviesByTitle := RecommendBasedOnTitle(&titleCgf, &recommendableTitles)
+	sort.SliceStable(similarMoviesByTitle, func(i, j int) bool {
+		return similarMoviesByTitle[i].MovieID < similarMoviesByTitle[j].MovieID
+	})
+	// Create a subset of movies, only keeping the movieIDs that are recommendable by Title-based correlation
+	recommendableMovies := map[int]model.Movie{cfg.Input: (*movies)[cfg.Input]}
+	for _, movie := range similarMoviesByTitle {
+		recommendableMovies[movie.MovieID] = ((*movies)[movie.MovieID])
+	}
+	movieCfg := config.Config{Similarity: cfg.Similarity, NumThreads: cfg.NumThreads}
+	similarMovies := findSimilarMovies(&movieCfg, cfg.Input, &recommendableMovies)
+	sort.SliceStable(similarMovies, func(i, j int) bool {
+		return similarMovies[i].MovieID < similarMovies[j].MovieID
 	})
 	// Merge the similarities of all 3 algorithms
 	idxMovie, idxTitle, idxTag := 0, 0, 0
@@ -40,7 +50,7 @@ func RecommendHybrid(cfg *config.Config, titles *map[int]model.MovieTitle, movie
 		similarMovieByTitle := similarMoviesByTitle[idxTitle]
 		similarMovieByTag := similarMoviesByTag[idxTag]
 		if similarMovie.MovieID == similarMovieByTitle.MovieID && similarMovie.MovieID == similarMovieByTag.MovieID {
-			// Combine the 3 similarity scores using 20%-40%-40% weights
+			// Combine the 3 similarity scores using 20%-40%-40% weights so that the upper limit of similarity remains 1.0
 			similarity := 0.2*similarMovie.Similarity*0.4*similarMovieByTitle.Similarity + 0.4*similarMovieByTag.Similarity
 			finalSimilarMovies = append(finalSimilarMovies, model.SimilarMovie{
 				MovieID:    similarMovie.MovieID,
